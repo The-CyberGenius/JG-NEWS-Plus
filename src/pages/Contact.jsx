f0212c003edce4e1068200b5b304afe538df4881
The-CyberGenius <sshivaprajapat@gmail.com>
2026-06-05 09:55:23 +0530

    update facebook social link

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -210,7 +210,7 @@ export default function Contact() {
                             </div>
                             <div style={{ display: 'flex', gap: '10px', flexWrap: 'wrap' }}>
                                 {[
-                                    { name: 'Facebook', color: '#4267B2', url: 'https://facebook.com/jgnewsplus' },
+                                    { name: 'Facebook', color: '#4267B2', url: 'https://www.facebook.com/profile.php?id=61590175844898' },
                                     { name: 'YouTube', color: '#FF0000', url: 'https://www.youtube.com/@MANOJ-1974-JG' },
                                     { name: 'Instagram', color: '#E1306C', url: 'https://www.instagram.com/jgnewsrajasthan/' },
                                     { name: 'X (Twitter)', color: '#000', url: 'https://x.com/jgnewslive' },
